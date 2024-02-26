b5acdf56b2cec2c4064e2c59c48db7fbda8c4c9b
Eliya <eliysamaea@gmail.com>
2024-02-26 15:25:21 +0200

    removing custom id from model

--- models/donation.model.js
+++ models/donation.model.js
@@ -2,7 +2,6 @@ const { Schema, model } = require("mongoose");
 
 const donationsSchema = new Schema(
   {
-    id: { type: String, required: true, unique: true },
     donorName: { type: String, required: true },
     amount: { type: String, required: true },
     location: { type: String, required: true },
