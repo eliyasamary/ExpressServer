d6b4a6243329c2449bbfa41a23c64653caa85910
Eliya <eliysamaea@gmail.com>
2024-02-28 09:36:27 +0200

    adding CORE

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const logger = require("morgan");
 const fs = require("fs");
 const path = require("path");
@@ -20,15 +21,9 @@ app.use(
     }),
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.set("Content-Type", "application/json");
-  next();
-});
+
+app.use(cors());
+
 app.use("/donations", donationsRouter);
 
 app.all("*", (req, res) => {
